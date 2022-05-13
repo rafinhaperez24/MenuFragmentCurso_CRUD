f97342e4b83f21ddc1ebf992d64b9870df2d4272
JosuePerez <jdps0497@gmail.com>
2022-05-13 13:30:07 -0600

    Correcciones

--- app/src/main/java/cr/ac/menufragmentcurso/AddEmpleadoFragment.kt
+++ app/src/main/java/cr/ac/menufragmentcurso/AddEmpleadoFragment.kt
@@ -1,20 +1,32 @@
 package cr.ac.menufragmentcurso
 
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.graphics.drawable.toBitmap
+import com.squareup.picasso.Picasso
 import cr.ac.menufragmentcurso.entity.Empleado
 import cr.ac.menufragmentcurso.repository.EmpleadoRepository
+import java.io.ByteArrayOutputStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val PICK_IMAGE = 100
 
 /**
  * A simple [Fragment] subclass.
@@ -24,6 +36,7 @@ private const val ARG_PARAM2 = "param2"
 class AddEmpleadoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1 : String? = null
+    lateinit var img_avatar : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,12 +52,16 @@ class AddEmpleadoFragment : Fragment() {
         // Inflate the layout for this fragment
         var view: View = inflater.inflate(R.layout.fragment_add_empleado, container, false)
         val btnAgregar = view.findViewById<Button>(R.id.agregarButton) as Button
+        img_avatar = view.findViewById(R.id.avatar)
 
         btnAgregar.setOnClickListener {
             val id1 = view.findViewById<TextView>(R.id.editID).text
             val nombre = view.findViewById<TextView>(R.id.editNombre).text
             val departamento = view.findViewById<TextView>(R.id.editDepartamento).text
             val puesto = view.findViewById<TextView>(R.id.editPuesto).text
+            img_avatar = view.findViewById(R.id.avatar)
+
+            var avatar = encodeImage(img_avatar.drawable.toBitmap())!!
 
 
             val builder = AlertDialog.Builder(context)
@@ -54,7 +71,7 @@ class AddEmpleadoFragment : Fragment() {
 
 
                     val empleado : Empleado  =  Empleado(null, id1.toString(),nombre.toString(),
-                        puesto.toString(),departamento.toString(),"")
+                        puesto.toString(),departamento.toString(),avatar)
                     EmpleadoRepository.instance.save(empleado)
                     val fragment : Fragment = CameraFragment.newInstance("Camera")
                     fragmentManager?.beginTransaction()?.replace(R.id.home_content, fragment)
@@ -69,7 +86,28 @@ class AddEmpleadoFragment : Fragment() {
             alert.show()
 
         }
+        img_avatar.setOnClickListener{
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, PICK_IMAGE)
+        }
         return view
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK){
+            val imageUrl = data?.data
+            Picasso.get().load(imageUrl).resize(120,120).centerCrop().into(img_avatar)
+        }
+    }
+
+    private fun encodeImage(bm: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val b = baos.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
+    private fun decodeImage (b64 : String): Bitmap {
+        val imageBytes = Base64.decode(b64, 0)
+        return  BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
     }
 
     companion object {
